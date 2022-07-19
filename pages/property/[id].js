21289d6e5694eba4c0d2a0f6eb82bb6bc3e26a98
Shane <shaneabh777@gmail.com>
2022-07-19 12:12:55 +0400

    Import error

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -5,8 +5,9 @@ import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 
-import { baseUrl, fetchApi } from '../../utils/fetchApi';
+
 import ImageScrollbar from '../../components/ImageScrollbar';
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
 
 const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
     <Box maxWidth='1000px' margin='auto' p='4'>
